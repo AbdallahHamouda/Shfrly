a107e65e12b03df67f884529a986acc34bcece32
AbdallahHamouda <abdallahhamouda4@gmail.com>
2020-11-05 14:29:32 +0200

    I hope it will work

---
+++ Desktop/dist/precache-manifest.fbc843cda4d4b7e1b5e44c566803dc54.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c258c98fa093e2beb7386e9da58d113",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "ed384a44bc51b788e8f5",
+    "url": "/css/about.d9b86199.css"
+  },
+  {
+    "revision": "c924065ee20d6764b56f",
+    "url": "/css/app.4833dcbe.css"
+  },
+  {
+    "revision": "520ec510274d2311a6ab7c6fcc27d214",
+    "url": "/img/Group 10.520ec510.png"
+  },
+  {
+    "revision": "6c906787b336dad7f1a3a203ec52b1fc",
+    "url": "/img/Group 22.6c906787.png"
+  },
+  {
+    "revision": "2b34bfbf906ce0ca7ba18f2cd8165167",
+    "url": "/img/Group 23.2b34bfbf.png"
+  },
+  {
+    "revision": "f0f86163fb75b8d78ccebb379ba6fd02",
+    "url": "/img/Group 8.f0f86163.png"
+  },
+  {
+    "revision": "36e9313136be7a952a94de31611604e8",
+    "url": "/img/email.36e93131.png"
+  },
+  {
+    "revision": "ad778fe009a608100cdc51f7a8638543",
+    "url": "/img/happy.ad778fe0.png"
+  },
+  {
+    "revision": "ab381c68aad811de4a903ee24981af6c",
+    "url": "/img/msg.ab381c68.png"
+  },
+  {
+    "revision": "fe0310d6f052409f36605e2380d49733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed384a44bc51b788e8f5",
+    "url": "/js/about.c106cc50.js"
+  },
+  {
+    "revision": "c924065ee20d6764b56f",
+    "url": "/js/app.5ce9a4f8.js"
+  },
+  {
+    "revision": "c907f6fee485d10b0eed",
+    "url": "/js/chunk-vendors.b837a614.js"
+  },
+  {
+    "revision": "e6c1cd1859a977948179f71f840f6229",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
